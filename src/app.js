2352a569d8961d68ec9353c745db9f8240e106a7
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-04-01 13:15:46 +0000

    first update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,59 @@
 import "bootstrap";
 import "./style.css";
 
+const who = [
+  "My dog",
+  "My cat",
+  "My neighbor",
+  "A mysterious stranger",
+  "The pizza delivery person",
+  "My old teacher",
+  "A time traveler",
+  "The ghost in my house",
+  "My evil twin",
+  "A superhero",
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const actions = [
+  "ate",
+  "destroyed",
+  "hid",
+  "stole",
+  "redesigned",
+  "erased",
+  "threw away",
+  "accidentally burned",
+  "spilled coffee on",
+  "mistook for trash",
+];
+
+const what = [
+  "my homework",
+  "my laptop",
+  "my car keys",
+  "my presentation",
+  "my phone",
+  "my wallet",
+  "my schedule",
+  "my alarm clock",
+  "my calendar",
+  "my project files",
+];
+
+const when = [
+  "while I was sleeping",
+  "right before the deadline",
+  "during an important meeting",
+  "this morning",
+  "when I wasn't looking",
+  "in a parallel universe",
+  "because Mercury is in retrograde",
+  "due to a temporary glitch in the Matrix",
+  "during a power outage",
+  "while I was making coffee",
+];
+
+
 
 window.onload = function() {
   //write your code here
